0e3fe2e9a24cccefe761dc2331a50fc1379f7b15
rickyrattlesnake <ricky.ratnayake@gmail.com>
2022-12-30 20:00:34 +1100

    wip:adding zod parsing

--- examples/example.ts
+++ examples/example.ts
@@ -1,8 +1,21 @@
 import * as path from 'path';
-import {parse} from '../src/index';
+import {load} from '../src/index';
 
-parse({path: path.join(__dirname, './')});
+const envs = load(
+  parser => {
+    return {
+      TEST_1: parser.number(),
+      TEST_2: parser.string(),
+      TEST_3: parser.boolean(),
+    };
+  },
+  {
+    envDirPath: path.join(__dirname, './'),
+  }
+);
 
 console.log('TEST_1', process.env.TEST_1);
 console.log('TEST_2', process.env.TEST_2);
 console.log('TEST_3', process.env.TEST_3);
+
+console.log('envs', envs);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 import * as dotenvFlow from 'dotenv-flow';
+import {z} from 'zod';
+import {
+  Parser,
+  ParserCommandType,
+  ParserConfigType,
+  ParserConfiguration,
+} from './parser';
 
-export const parse = ({path}: {path: string}) =>
-  dotenvFlow.config({
-    path: path,
-  });
+interface ParseOptions {
+  envDirPath?: string;
+}
+
+type LoadConfig = (c: ParserConfigType) => Record<string, ParserCommandType>;
+
+export const load = (config: LoadConfig, options?: ParseOptions) => {
+  // const dotenvFiles = dotenvFlow.listDotenvFiles(envDirPath || './');
+  // console.log(dotenvFiles);
+
+  const dotenvVars = dotenvFlow.config({path: options?.envDirPath}).parsed;
+  console.log('dotenvVars', dotenvVars);
+  // console.log(process.env);
+
+  const parserConfigResult = config(new ParserConfiguration());
+  const parser = Parser.fromConfigObject(parserConfigResult);
+  return parser.parse(process.env);
+};
